bdf55b4588b3c583d92a2842b4b3e039141c1175
Brycen Dorsay <dorsay@live.ca>
2013-12-24 02:08:55 -0800

    Initial Commit Decimal to Binary and Decimal to Hexadecimal conversions working

---
+++ src/com/brycendorsay/numberconverter/BinaryFragment.java
@@ -0,0 +1,58 @@
+package com.brycendorsay.numberconverter;
+
+import com.brycendorsay.numberconverter.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BinaryFragment extends Fragment {
+
+	private TextView output;
+	private EditText input;
+	private Button convert;
+	private String in;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_binary_layout, container, false);
+
+		input = (EditText) rootView.findViewById(R.id.decimalToBinaryInput);
+		output = (TextView) rootView.findViewById(R.id.decimalToBinaryOutput);
+		convert = (Button) rootView.findViewById(R.id.binaryConvertButton);
+
+		convert.setOnClickListener(
+				new View.OnClickListener() {
+					public void onClick(View view) {
+						in = input.getText().toString();
+						output.setText("" + convertValue(in));
+					}
+				});
+
+		return rootView;
+	}
+
+	private String convertValue(String x) {
+		int temp = Integer.parseInt(x);
+		int mod;
+		String newString;
+		if(temp <= 0)
+			return "";
+
+		mod = temp % 2;
+		
+		newString = new String(Integer.toString(temp/2));
+		
+		return "" + convertValue(newString) + mod;
+	}
+
+}
---
+++ src/com/brycendorsay/numberconverter/HexadecimalFragment.java
@@ -0,0 +1,93 @@
+package com.brycendorsay.numberconverter;
+
+import com.brycendorsay.numberconverter.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HexadecimalFragment extends Fragment {
+
+	private TextView output;
+	private EditText input;
+	private Button convert;
+	private String in;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_hexadecimal_layout, container, false);
+
+		input = (EditText) rootView.findViewById(R.id.decimalToHexInput);
+		output = (TextView) rootView.findViewById(R.id.decimalToHexOutput);
+		convert = (Button) rootView.findViewById(R.id.hexConvertButton);
+
+		
+
+		convert.setOnClickListener(
+				new View.OnClickListener() {
+					public void onClick(View view) {
+						in = input.getText().toString();
+						output.setText("" + convertValue(in));
+					}
+				});
+
+
+
+
+		return rootView;
+	}
+
+	private String convertValue(String x) {
+		int temp = Integer.parseInt(x);
+		int mod = 0;
+		String modded = "";
+		String newString;
+		
+		if(temp <= 0)
+			return "";
+
+		mod = temp % 16;
+		
+		if(mod >= 10) {
+			
+			switch(mod) {
+			case 10:
+				modded = "A";
+				break;
+			case 11:
+				modded = "B";
+				break;
+			case 12:
+				modded = "C";
+				break;
+			case 13:
+				modded = "D";
+				break;
+			case 14:
+				modded = "E";
+				break;
+			case 15:
+				modded = "F";
+				break;
+			default:
+				modded = "Z";
+				break;
+			}
+			
+		} else if(mod < 10) {
+			modded = Integer.toString(mod);
+		}
+		
+		newString = new String(Integer.toString(temp/16));
+		
+		return "" + convertValue(newString) + modded;
+	}
+
+}
